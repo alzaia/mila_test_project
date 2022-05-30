321739117b9c8f3d20e7646e2f8298ec1dc40ec5
Aldo Zaimi <aldozaimi@MacBook-Pro-de-Mila.local>
2022-05-30 14:23:07 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='mila_test_project',
+    version='0.0.1',
+    packages=find_packages(include=['mila_test_project', 'mila_test_project.*']),
+    python_requires='>=3.8',
+    install_requires=[
+        'flake8',
+        'flake8-docstrings',
+        'gitpython',
+        'tqdm',
+        'jupyter',
+        'mlflow==1.15.0',
+        'orion>=0.1.14',
+        'pyyaml>=5.3',
+        'pytest>=4.6',
+        'pytest-cov',
+        'sphinx',
+        'sphinx-autoapi',
+        'sphinx-rtd-theme',
+        'sphinxcontrib-napoleon',
+        'sphinxcontrib-katex',
+        'recommonmark',
+        'torch==1.9.0', 'pytorch_lightning==1.2.7'],
+    entry_points={
+        'console_scripts': [
+            'main=mila_test_project.main:main'
+        ],
+    }
+)
